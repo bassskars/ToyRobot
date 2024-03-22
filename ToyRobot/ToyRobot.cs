0123a7bb0342b584943a51a0a6ce9ade8aff32e1
Baskar Somasundaram <bassskar.s@gmail.com>
2024-03-22 19:05:37 +0530

    Fixed the UnitTest Issue

--- ToyRobot/ToyRobot.cs
+++ ToyRobot/ToyRobot.cs
@@ -16,13 +16,13 @@
             _maxY = maxY;
         }
 
-        public void Place(int x, int y, string direction)
+        public virtual void Place(int x, int y, string direction)
         {
             if (IsValid(x, y, direction))
                 (X, Y, Facing) = (x, y, direction);
         }
 
-        public void Move()
+        public virtual void Move()
         {
             if (Facing == Direction.North && IsValid(X, Y + 1, Facing)) Y += 1;
             else if (Facing == Direction.East && IsValid(X + 1, Y, Facing)) X += 1;
@@ -30,11 +30,11 @@
             else if (Facing == Direction.West && IsValid(X - 1, Y, Facing)) X -= 1;
         }
 
-        public void Left() => Facing = Facing.IsValid() ? Facing.RotateLeft() : null;
+        public virtual void Left() => Facing = Facing.IsValid() ? Facing.RotateLeft() : null;
 
-        public void Right() => Facing = Facing.IsValid() ? Facing.RotateRight() : null;
+        public virtual void Right() => Facing = Facing.IsValid() ? Facing.RotateRight() : null;
 
-        public string Report() => IsValid(X, Y, Facing) ? $"{X},{Y},{Facing}" : null;
+        public virtual string Report() => IsValid(X, Y, Facing) ? $"{X},{Y},{Facing}" : null;
 
         private bool IsValid(int? x, int? y, string direction)
             => x >= 0 && x < _maxX && y >= 0 && y < _maxY && direction.IsValid();
